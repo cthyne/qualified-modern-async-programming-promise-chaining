073c76137f839527dc8777492b8b9d6019c11990
Ciara Thyne <ciara.thyne@gmail.com>
2022-11-11 11:47:36 -0800

    revised solution

--- src/main.js
+++ src/main.js
@@ -1,31 +1,21 @@
 const axios = require("../utils/axios");
-// const BASE_URL = "http://localhost:5000";
+const BASE_URL = "http://localhost:5000";
 
 
 function updateIfExists(id, body) {
-  const url = `http://localhost:5000/constellations/${id}`;
-  return axios
-  .get(url)
+  const url = `${BASE_URL}/constellations/${id}`;
+  return axios.get(url)
   .then(({data}) => {
-    if (!{data}) throw 'Constellation does not exist' 
-      return axios.put(url, body);
+    if(!{data}) throw `Constellation with this ${id} does not exist`;
+    return axios.put(url, body)
   })
-  .then(({ data }) => {
+  .then(({data}) => {
     return data
-  })
+  }) 
   .catch((error) => {
-    return error.message
-  });
-}
-
-const body = {
-  "id": "UEUrlfX",
-  "name": "Columba",
-  "meaning": "Dove",
-  "starsWithPlanets": 3,
-  "quadrant": "SQ1"
-}
-updateIfExists("UEUrlfX", body);
+    return error.message;
+  })
+};
 
 module.exports = {
   updateIfExists,
